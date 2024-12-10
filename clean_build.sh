f030bb2575a0340a1594ca74df08a5b847f8c973
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-12-10 16:15:09 -0500

    check

---
+++ clean_build.sh
@@ -0,0 +1,15 @@
+rm -r build
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+
+
+
+echo "file_path,branch_id,source_line,destination_line" > branch_data.csv
+
+clang -g -o0 -S -emit-llvm -c tests/red_black_tree.c -o IR_Outputs/red_black_tree_1.ll
+
+clang -g -o0 -fpass-plugin=build/libSeminalInputFeaturesAnalysis.so -emit-llvm -c tests/red_black_tree.c -o IR_Outputs/red_black_tree_2.ll
+opt -loadf-pass-plugin build/BranchPass/libBranchPass.so -passes=branch-analysis-pass -disable-output IR_Outputs/red_black_tree_2.ll
